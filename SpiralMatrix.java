c5f03ae8cbf2d946b109b3fed28c225d0690349c
Aditya Raj <aditya998887766@gmail.com>
2026-04-06 12:46:46 +0530

    travel in matric
    travel in matric

---
+++ SpiralMatrix.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class SpiralMatrix {
+    // most important questation******************************************
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matrix = new int[3][3];
+
+        // Input 
+        System.out.println("Enter 3x3 matrix elements:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int top = 0, bottom = 2;
+        int left = 0, right = 2;
+
+        System.out.print("Spiral Order: ");
+
+        while (top <= bottom && left <= right) {
+
+            // Left to Right
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            // Top to Bottom
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            // Right to Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            // Bottom to Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+
+        sc.close();
+    }
+}
