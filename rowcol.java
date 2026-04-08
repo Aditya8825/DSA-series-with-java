03e92ed166a9748173e9b10be878e2b93eec0e50
Aditya Raj <aditya998887766@gmail.com>
2026-04-08 23:58:34 +0530

    row wise and column wise print

---
+++ rowcol.java
@@ -0,0 +1,16 @@
+public class rowcol {
+  public static void main(String[] args) {
+    int  [][] arr={
+        {1,2,3,4,5},
+        {6,7,8,9,1},
+        {9,8,7,6,5}
+
+    };
+     for(int j=0; j<arr[0].length; j++){
+        for(int i=0 ; i<arr.length; i++){
+            System.out.print(arr[i][j]+" ");
+        }
+        System.out.println();
+     }
+  }  
+}
