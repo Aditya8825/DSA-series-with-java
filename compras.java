ecbb1ea44624b4e1b204b6e02858070c2d539c74
Aditya Raj <aditya998887766@gmail.com>
2026-04-08 22:26:11 +0530

    som of string question

---
+++ compras.java
@@ -0,0 +1,23 @@
+// import java.util.*;
+// public class compras {
+//   public static void main(String[] args) {
+//     Scanner sc= new Scanner((System.in));
+//     System.out.println("enter string:");
+
+//     String str=sc.next();
+    
+//     String result=" ";
+//     int count=1;
+
+//     for(int i=0; i<str.length(); i++)
+// {
+//     if(i<str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+//         count++;
+//     }else{
+//         result =result+str.charAt(i)+count;
+//         count=1;
+//     }
+// }
+// System.out.println("compressed:"+result);
+//   }  
+// }
