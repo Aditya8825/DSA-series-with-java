357d4bef452b4606193e4595ecdd4251f80be261
Aditya Raj <aditya998887766@gmail.com>
2026-04-08 23:57:16 +0530

    2darray/matric

---
+++ Matrix.java
@@ -0,0 +1,28 @@
+
+// import java.util.*;
+
+// public class Matrix {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int[][] arr = new int[3][4];
+
+//         System.out.println("Enter elements:");
+
+//         // Input
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = 0; j < arr[0].length; j++) {
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+
+//         // Output
+//         System.out.println("Matrix is:");
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = 0; j < arr[0].length; j++) {
+//                 System.out.print(arr[i][j] + " ");
+//             }
+//             System.out.println(); // move to next row
+//         }
+//     }
+// }
