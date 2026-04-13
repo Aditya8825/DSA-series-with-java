5a22e9650627cf688611ec6895c9280be0ba4ebf
Aditya Raj <aditya998887766@gmail.com>
2026-04-13 23:08:13 +0530

    found key element present or not
    found key elements

---
+++ found.java
@@ -0,0 +1,14 @@
+// public class found {
+//     static boolean foundnum(int []arr,int i,int key){
+        
+//         if(i==arr.length) return false;
+//         if(arr[i]==key) return true;
+//         return foundnum(arr, i+1, key);
+//     }
+//     public static void main(String[] args) {
+//         int []arr={1,2,2,4,2,6,7};
+//         int key =1;
+//         int i=0;
+//        System.out.println(foundnum(arr, i, key));
+//     }
+// }
