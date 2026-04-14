dd8fcc9e0485db77a415411cfab0019c8055b96a
Aditya Raj <aditya998887766@gmail.com>
2026-04-14 17:37:22 +0530

    positive element in entire string
    positive element

---
+++ positive.java
@@ -0,0 +1,20 @@
+// public class positive {
+
+//     static boolean positivenum(int[] arr, int i) {
+
+       
+//         if (i == arr.length) return true;
+
+      
+//         if (arr[i] < 0) return false;
+
+      
+//         return positivenum(arr, i + 1);
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 2, 4, 2, 6, 7};
+
+//         System.out.println(positivenum(arr, 0));
+//     }
+// }
