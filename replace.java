2c59d09a7ab6ae8bf1f133e6964b539c4b18b763
Aditya Raj <aditya998887766@gmail.com>
2026-04-14 17:38:14 +0530

    replace element by change thier index value
    replace element

---
+++ replace.java
@@ -0,0 +1,17 @@
+// public class replace {
+//     static void replaceofnum(int [] arr,int i){
+//         if(i==arr.length) return;
+
+//         if(arr[i]==2){
+//             arr[i]=5;
+//         }
+//         replaceofnum(arr, i+1);
+//     }
+// public static void main(String[] args) {
+//     int [] arr={1,2,2,4,2,6,7};
+//     replaceofnum(arr, 0);
+//     for(int num : arr){
+//         System.out.println(num+" ");
+//     }
+// }
+// }
