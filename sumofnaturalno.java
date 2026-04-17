9afad43bf025670dd7615180c12943d03cb0cea5
Aditya Raj <aditya998887766@gmail.com>
2026-04-17 23:38:22 +0530

    sum of n natural number
    sum of n natural simple code

---
+++ sumofnaturalno.java
@@ -0,0 +1,12 @@
+// public class sumofnaturalno {
+//      public static  int sum(int n){
+//         if(n==0){
+//             return 0;
+//         }
+//         return n + sum(n-1);
+//      }
+//      public static void main(String[] args) {
+//         int n=10;
+//         System.out.println("sum:"+sum(n));
+//      }
+// }
