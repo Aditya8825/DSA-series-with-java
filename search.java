2b61401f15dbd5b9c086569727f1bfaf9ff580aa
Aditya Raj <aditya998887766@gmail.com>
2026-04-19 21:59:36 +0530

    Add files via upload

---
+++ search.java
@@ -0,0 +1,29 @@
+
+
+// public class search {
+//     public static void main(String[] args) {
+//         int []arr={1,2,3,4,5,6,7,8};
+//         int target=8;
+//         System.out.println(exists(arr.target,0));
+//     }
+//     public static boolean exists(int[]arr,int target, int idx){
+//         if(idx == arr.length) return false;
+//         if(arr[idx] == target) return true;
+//         return exists(arr, target, idx+1);
+//     }
+// }
+
+
+// public class search {
+//     public static void main(String[] args) {
+//         int[] arr = {1,2,3,4,5,6,7,8};
+//         int target = 8;
+//         System.out.println(exists(arr, target, 0));
+//     }
+
+//     public static boolean exists(int[] arr, int target, int idx) {
+//         if (idx == arr.length) return false;
+//         if (arr[idx] == target) return true;
+//         return exists(arr, target, idx + 1);
+//     }
+// }
