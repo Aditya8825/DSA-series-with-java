a89e221dd886f435c4889110e120a1bce7655f2b
Aditya Raj <aditya998887766@gmail.com>
2026-04-22 23:07:41 +0530

    convering f string into binary number'

---
+++ binar.java
@@ -0,0 +1,20 @@
+
+
+
+public class binar {
+    
+public static int binarysearch(int []arr,int left, int right,int target)
+if(left>right){
+    return -1;
+}
+int mid=left+(right-left)/2;
+if (arr[mid]==target){
+    return mid;
+}
+if(target<arr[mid])
+
+    public static void main(String[] args) {
+        int [] arr={2,3,4,6,7,89};
+        int target=89;
+    }
+}
