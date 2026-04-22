fc6545f260b3530ba78fb33ca91ac27488458a00
Aditya Raj <aditya998887766@gmail.com>
2026-04-22 23:06:39 +0530

    sum of strinf

---
+++ sumof.java
@@ -0,0 +1,32 @@
+// import java.util.*;
+// public class sumof{
+//     public static void main(String[] args) {
+//     //     int []arr={10,20,30,40};
+
+//     //     int sum=0;
+//     //     for(int n: arr){
+//     //         sum+= n;
+//     //     }
+//     //     System.out.println("sum"+sum);
+//     // }
+// // }
+
+
+// //take input from usser
+// Scanner sc=new Scanner(System.in);
+// System.out.print("enter size:");
+// int n=sc.nextInt();
+
+// int []arr=new int[n];
+// int sum=0;
+
+
+// System.out.print("array element:");
+// for(int i=0; i<n; i++){
+//     arr[i]=sc.nextInt();
+//     sum+=arr[i];
+// }
+// System.out.println("sum:"+sum);
+//     }
+// }
+
