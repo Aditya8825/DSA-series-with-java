6e20d1d70a5f45d1723616d8a513a8e1ce8f50df
Aditya Raj <aditya998887766@gmail.com>
2026-04-27 21:15:32 +0530

    Add files via upload

---
+++ sticklerthief.java
@@ -0,0 +1,22 @@
+// // package DYANMIC WITH RECURSION;
+
+// public class sticklerthief {
+//     static int maxlot(int []arr,int n){
+//         //base case
+//         if(n<0) return 0;
+//         if(n==0) return arr[n];
+
+//         //include current house+skip one
+// //         int include=arr[n]+maxlot(arr,n-2);
+
+// //         //exclude currenthouse
+// //         int exclude=maxlot(arr,n-1);
+// //         return Math.max(include,exclude);
+// //     }
+// //     public static void main(String[] args) {
+// //         int[] arr={5,5,10,100,10,5};
+// //         int n =arr.length;
+// //         System.out.println("maximum loot:"+maxlot(arr,n-1 ));
+
+// //     }
+// // }
