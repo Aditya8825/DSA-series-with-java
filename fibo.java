234816a945a98d59d55b537fdab0bf10cac2d466
Aditya Raj <aditya998887766@gmail.com>
2026-04-27 21:14:19 +0530

    Add files via upload

---
+++ fibo.java
@@ -0,0 +1,21 @@
+//  public class pratic{
+//     static int fibo(int n){
+//         if(n<=1){
+//             return n;
+//         }
+//         return fibo(n-1)+fibo(n-2);
+//     }
+//       static int sumoffibo(int n){
+//         int sum=0;
+//         for(int i=0; i<n; i++){
+//             sum +=fibo(i);
+//         }
+//         return sum;
+//     //   }
+//     // public static void main(String[] args) {
+//     //     int n=10;
+//     //     // for(int i=0; i<n; i++){
+//     //         System.out.println("Sum of first " + n + " Fibonacci numbers: " + sumoffibo(n));
+//     //         // System.out.println(sumoffibo(n));
+//     //     }
+//     // }
