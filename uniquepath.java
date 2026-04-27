553226aec63fce3f65babde2f28412241e9a498f
Aditya Raj <aditya998887766@gmail.com>
2026-04-27 21:16:06 +0530

    Add files via upload

--- uniquepath.java
+++ uniquepath.java
@@ -1,13 +1,29 @@
-public class uniquepath {
-    public static int uniquepath(int m, int n){
-        if(m==1  || n==1) return 1;
-        return uniquepath(m-1, n) + uniquepath(m, n-1);
+// import java.util.*;
 
-    }
-    public static void main(String[] args) {
-        int m=2;
-        int n=2;
-        System.out.println(uniquepath(m,n));
-    }
-    
-}
+// public class uniquepath {
+//     static int uniquepaths(int m, int n){
+//         //base case out of frid 
+//         if(m<=0 || n<=0){
+//             return 0;
+//         }
+//         // base case  strt cell
+//         if(m==1 && n==1){
+//             return 1;
+//         }
+//         // move up +move left
+//         int up=uniquepaths(m-1, n);
+//         int left=uniquepaths(m, n-1);
+//         return up+left;
+
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter m:");
+//         int m=sc.nextInt();
+
+//         System.out.println("enter n:");
+//         int n=sc.nextInt();
+
+//         System.out.println("unique path:"+uniquepaths(m,n));
+//     }
+// }
