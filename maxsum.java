9a161fee94dadb436df9ce8b27a145dad942091e
Aditya Raj <aditya998887766@gmail.com>
2026-04-27 21:15:07 +0530

    Add files via upload

---
+++ maxsum.java
@@ -0,0 +1,23 @@
+
+
+// public class maxsum {
+//     public static void main(String[] args) {
+//         int[][] arr={
+//             {1,2,3,4},
+//             {5,6,7,8},
+//             {5,4,3,2},
+//             {9,8,7,5}
+            
+//         };
+//         int n=arr.length;
+//         int result=-100000;
+//         for(int i=0; i<n; i++){
+//             int value= maxsum(arr,0,i,n);
+//            if(value >result){
+//             result=value;
+//            }
+//         }
+
+//         System.out.println("maximum path sum:"+result);
+//         }
+// }
